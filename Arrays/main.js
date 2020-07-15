29cd487e2a16c0ba1b3a1679b9b6bcbaced5219e
unknown <himanshu25031991@gmail.com>
2020-07-15 22:40:15 +0530

    array

---
+++ Arrays/main.js
@@ -0,0 +1,23 @@
+const string=['a','b','c','d'];
+console.log(string[2]);
+console.log(string);
+
+console.log('push e');
+string.push('e');//O(1)
+console.log(string);
+
+console.log('pop e');
+string.pop();//O(1)
+console.log(string);
+
+console.log('pop d');
+string.pop();//O(1)
+console.log(string);
+
+console.log('push x at beginning');
+string.unshift('x');//O(n)
+console.log(string);
+
+console.log('push alien at position 2');
+string.splice(2,0,"alien");
+console.log(string);//O(n)
