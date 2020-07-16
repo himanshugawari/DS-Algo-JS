4dae3118eeefcf6fee6475398f1e4b4fed171952
unknown <himanshu25031991@gmail.com>
2020-07-16 08:24:58 +0530

    reverse a string

---
+++ Arrays/ReverseAString.js
@@ -0,0 +1,30 @@
+function reverse(str) {
+  // check input
+  if (!str || str.length < 2 || typeof str !== "string") {
+    return "Invalid String";
+  }
+  const backwardArray = [];
+  const totalItems = str.length - 1;
+  for (let i = totalItems; i >= 0; i--) {
+    backwardArray.push(str[i]);
+  }
+  console.log(backwardArray);
+  return backwardArray.join("");
+}
+
+function reverse2(str) {
+  // check input
+  if (!str || str.length < 2 || typeof str !== "string") {
+    return "Invalid String";
+  }
+  return str.split("").reverse().join("");
+}
+
+const reverse3 = (str) => str.split("").reverse().join("");
+
+const reverse4 = (str) => [...str].reverse().join("");
+
+console.log(reverse("hello"));
+console.log(reverse2("hello"));
+console.log(reverse3("hello"));
+console.log(reverse4("hello"));
