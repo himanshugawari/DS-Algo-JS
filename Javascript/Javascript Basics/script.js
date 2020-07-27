8958c665008b2ae53715f91d9a2c25610343fdd4
unknown <himanshu25031991@gmail.com>
2020-07-27 09:28:32 +0530

    primitive types

---
+++ Javascript/Javascript Basics/script.js
@@ -0,0 +1,35 @@
+console.log("My name is Himanshu Gawari");
+
+// let const var
+
+let myNmae = "Himanshu";
+const PI = 3.141;
+var iLike = "pizza";
+
+// PI = 3.4; // type error : assignment to constant variable
+myNmae = "something";
+iLike = "pani puri";
+
+let string = "This is a string";
+let number = 7;
+let bool = false;
+let array = [1, 2, 3, 4, 5, "a", "b", "himanshu", "car"];
+let _undefined = undefined;
+let _null = null;
+let _NaN = NaN;
+let object = {
+  keyName: "value1",
+  "key-Name": "value2",
+};
+
+console.log(string);
+console.log(number);
+console.log(bool);
+console.log(array);
+console.log(array[0], array[5], array[7]);
+console.log(_undefined);
+console.log(_null);
+console.log(_NaN);
+console.log(object);
+console.log(object["keyName"], object["key-Name"]);
+console.log(object.keyName, object["key-Name"]);
