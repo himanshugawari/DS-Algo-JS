28f9ea35538f9b7657a8395637963f5d364c3cc9
unknown <himanshu25031991@gmail.com>
2020-07-27 09:32:42 +0530

    DOM manipulation

---
+++ Javascript/Javascript Basics/script11.js
@@ -0,0 +1,6 @@
+var name = document.getElementById("name").innerText;
+console.log(name);
+
+document.getElementById("name").innerText = "My name is changed to newHimanshu";
+var newName = document.getElementById("name").innerText;
+console.log(newName);
